8ecd5989281452884612a1d0ec2678db08cfc420
Martín Perez <145036089+martinperez0512@users.noreply.github.com>
2024-03-27 11:41:17 -0400

    Update a2_q1.py

--- a2_q1.py
+++ a2_q1.py
@@ -6,7 +6,7 @@ Email: 22mp4@queensu.ca
 Date: 2023-02-01
 I confirm that this assignment solution is my own work and conforms to Queen’s standards of Academic Integrity
 """
-from functions import all_odd_or_even  # import function from the other file
+from a2_functions import all_odd_or_even  # import function from the other file
 
 
 def main():
