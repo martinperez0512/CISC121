f0c52666e6e0a937f46d65c5f21e7718ba87279d
Martín Perez <145036089+martinperez0512@users.noreply.github.com>
2024-04-25 11:25:14 -0400

    Update a5_q1.py

--- a5_q1.py
+++ a5_q1.py
@@ -6,7 +6,7 @@ Email: 22mp4@queensu.ca
 Date: 2023-03-29
 I confirm that this assignment solution is my own work and conforms to Queen’s standards of Academic Integrity
 """
-import hotel_classes
+import a5_hotel_classes
 import random
 
 
@@ -20,7 +20,7 @@ def create_standard_rooms():
     """
     standard_rooms = []  # create empty list
     for i in range(1, 90):  # iterate over the amount of rooms
-        stand_room = hotel_classes.StandardRoom(i + 100, True)  # create room object
+        stand_room = a5_hotel_classes.StandardRoom(i + 100, True)  # create room object
         standard_rooms.append(stand_room)  # add to list
     return standard_rooms  # return list
 
@@ -32,7 +32,7 @@ def create_suites():
     """
     suites = []  # create empty list
     for i in range(1, 20):  # iterate over the amount of rooms
-        suite_room = hotel_classes.Suite(i + 200, True)  # create room object
+        suite_room = a5_hotel_classes.Suite(i + 200, True)  # create room object
         suites.append(suite_room)  # add to list
     return suites  # return list
 
@@ -44,7 +44,7 @@ def create_taylor_suites():
     """
     taylor_suites = []  # create empty list
     for i in range(1, 14):  # iterate over the amount of rooms
-        taylor_suite = hotel_classes.TaylorSuite(i + 300, True)  # create room object
+        taylor_suite = a5_hotel_classes.TaylorSuite(i + 300, True)  # create room object
         taylor_suites.append(taylor_suite)  # add to list
     return taylor_suites  # return list
 
@@ -65,24 +65,24 @@ def main():
             guest_name = input("Your name: ")
             guest_room_type = input("What kind of room you would like? (Standard, Suite or Taylor Suite): ").lower()
             guest_check_out_date = input("Until what date do you plan on staying? (enter as YYYY-MM-DD) ")
-            guest_1 = hotel_classes.Guest(guest_name, guest_check_out_date)  # create guest object
+            guest_1 = a5_hotel_classes.Guest(guest_name, guest_check_out_date)  # create guest object
 
             if guest_room_type == "standard":  # if user chooses standard room
-                standard = hotel_classes.StandardRoom(random.choice(standard_rooms), True)  # pick random room
+                standard = a5_hotel_classes.StandardRoom(random.choice(standard_rooms), True)  # pick random room
                 if standard.is_available():  # if room is available
                     standard.guest_check_in(guest_1)  # check in with guest info
                     print(standard.room_number)  # show guest their room number
                 else:  # if room is not available
                     print(f"Sorry, no {guest_room_type} rooms are available")
             elif guest_room_type == "suite":  # if user chooses suite
-                suite = hotel_classes.Suite(random.choice(suites), True)  # pick random room
+                suite = a5_hotel_classes.Suite(random.choice(suites), True)  # pick random room
                 if suite.is_available():  # if room is available
                     suite.guest_check_in(guest_1)  # check in with guest info
                     print(suite.room_number)  # show guest their room number
                 else:  # if room is not available
                     print(f"Sorry, no {guest_room_type} rooms are available")
             elif guest_room_type == "taylor suite":  # if user chooses taylor suite
-                taylor_suite = hotel_classes.TaylorSuite(random.choice(taylor_suites), True)  # pick random room
+                taylor_suite = a5_hotel_classes.TaylorSuite(random.choice(taylor_suites), True)  # pick random room
                 if taylor_suite.is_available():  # if room is available
                     taylor_suite.guest_check_in(guest_1)  # check in with guest info
                     print(taylor_suite.room_number)  # show guest their room number
@@ -93,20 +93,20 @@ def main():
                 return  # return
 
             print("Would you like any additional services? \nSnacks = $10 \nTaxi = $20\nMovie = $5 ")  # print services
-            snacks = hotel_classes.Service("Snacks", 10)  # create service object
-            taxi = hotel_classes.Service("Taxi", 20)  # create service object
-            movie = hotel_classes.Service("Movie", 5)  # create service object
+            snacks = a5_hotel_classes.Service("Snacks", 10)  # create service object
+            taxi = a5_hotel_classes.Service("Taxi", 20)  # create service object
+            movie = a5_hotel_classes.Service("Movie", 5)  # create service object
             guest_service = input("Enter services you would like (put none if you don't want services): ").lower()
             if "snacks" in guest_service:  # check if user wants this service
-                hotel_classes.Guest.add_service(guest_1, snacks)  # add to guest object
+                a5_hotel_classes.Guest.add_service(guest_1, snacks)  # add to guest object
             if "taxi" in guest_service:  # check if user wants this service
-                hotel_classes.Guest.add_service(guest_1, taxi)  # add to guest object
+                a5_hotel_classes.Guest.add_service(guest_1, taxi)  # add to guest object
             if "movie" in guest_service:  # check if user wants this service
-                hotel_classes.Guest.add_service(guest_1, movie)  # add to guest object
+                a5_hotel_classes.Guest.add_service(guest_1, movie)  # add to guest object
 
             guest_total = input("Would you like to see your total bill? (yes/no) ").lower()  # ask if user wants bill
             if guest_total == "yes":  # if user wants to see bill. Room costs are not included (professor said it's OK)
-                hotel_classes.Guest.send_bill(guest_1)  # call method with guest input and print bill
+                a5_hotel_classes.Guest.send_bill(guest_1)  # call method with guest input and print bill
             else:  # if user does not want to see bill
                 print("OK, enjoy your stay!")
 
